682c8fb4848d8e9797ccd27a5eced066ff9abe43
sachaamm <sachaamm@gmail.com>
2020-12-01 10:17:53 +0100

    Pong IA

---
+++ Assets/Scripts/PongIA.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PongIA : MonoBehaviour
+{
+    public GameObject ball;
+    private Vector3 ballPos;
+
+    public float rangeToTrackBall = 5;
+    public float paddleSpeed = 0.5f; 
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Suivre le mouvement de la balle
+        // en se mettant sur la valeur y de la balle
+        ballPos = ball.transform.position;
+        
+        if (transform.position.x - ballPos.x < rangeToTrackBall)
+        {
+            FollowBall();
+        }
+        
+     }
+
+    void FollowBall()
+    {
+        // On met a jour iaPaddlePos
+        Vector3 iaPaddlePos = transform.position;
+        Vector3 goalPaddlePos = 
+            new Vector3(iaPaddlePos.x, ballPos.y, iaPaddlePos.z);
+
+        Vector3 lerpIaPaddlePos =
+            Vector3.Lerp(iaPaddlePos, goalPaddlePos, paddleSpeed * Time.deltaTime);
+        
+        // On assigne (=) transform position a lerpIaPaddlePos
+        transform.position = lerpIaPaddlePos;
+        
+    }
+}
